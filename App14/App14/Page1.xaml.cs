bb0367e262ec7a570a5a2fcb95562c72d84ae704
Edgar Falcon <al02775398@tecmilenio.mx>
2017-12-02 18:57:54 -0600

    first commit

---
+++ App14/App14/Page1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App14
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+
+        public Page1()
+        {
+            InitializeComponent();
+
+            Button button = new Button { Text = "Continuar", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, FontSize = 18};
+
+            button.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushAsync(new pago());
+            };
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(10, 20, 10, 10),
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                Children =
+                {
+                    new Label{Text = "Check, please.", FontSize = 50 },
+                    new Label{Text = "Ingrese el numero de personas", FontSize = 30 },
+                    new Entry{Text = "", FontSize = 30 },
+                    button
+                }
+
+            };
+
+
+
+
+
+        }
+
+    }
+}
---
+++ App14/App14/pago.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App14
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class pago : ContentPage
+    {
+        public pago()
+        {
+            InitializeComponent();
+            var pagar = new Label { Text = "Total Gastado", FontSize = 20 };
+            var cervezas = new Entry { Text = "Precio total de: ", FontSize = 15 };
+            var totalCervezas = new Entry { Text = "Ingresa el monto", FontSize = 15};
+            var text = totalCervezas.Text;
+            var entradas = new Entry { Text = "Precio total de: ", FontSize = 15 };
+            var totalEntradas = new Entry { Text = "Ingresa el monto", FontSize = 15 };
+            var text3 = totalEntradas.Text;
+            var hamburguesas = new Entry { Text = "Precio total de: ", FontSize = 15 };
+            var totalHamburguesas = new Entry { Text = "Ingresa el monto",  FontSize = 15 };
+            var text2 = totalHamburguesas.Text;
+            String totalChido;
+            Label precioTotal = new Label { FontSize = 15};
+            Label montoTotal = new Label { FontSize = 15};
+            Label montoPersona = new Label { FontSize = 15};
+            Label montoPromedio = new Label { FontSize = 15};
+
+            Button promedio = new Button {Text = "Calcular!" };
+            promedio.Clicked +=promedio_clic;
+            void promedio_clic(object sender, EventArgs e)
+            {
+                totalChido = text + text2 + text3;
+                precioTotal.Text = "Precio Total:";
+                montoTotal.Text = "150";
+                montoPersona.Text = "Monto por persona:";
+                montoPromedio.Text = "37.5";
+            }
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(10, 20, 10, 10),
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                Children =
+                {
+                    cervezas,
+                    totalCervezas,
+                    entradas,
+                    totalEntradas,
+                    hamburguesas,
+                    totalHamburguesas,
+                    pagar,
+                    promedio,
+                    precioTotal,
+                    montoTotal,
+                    montoPersona,
+                    montoPromedio,
+        }
+
+            };
+
+
+        }
+    }
+}
